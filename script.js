56ad8610cb7954f12a7e8b5fc6d02acfb311b44d
Vivek Kumar <vk13114416@gmail.com>
2025-07-27 12:20:14 +0000

    Added Play image.
    Added logic to the game including the winning and restart.
    All done using module pattern design.

--- script.js
+++ script.js
@@ -10,27 +10,27 @@ function gameGrid() {
 
     const getGrid = () => grid;
 
-    const markMove = (row, column, token) => {
+    const markMove = (row, column, token, move) => {
         
-        if (grid[row][column].getToken() !== "_") {
-            console.log("Wrong Move!!!");
-            return false;
+        if (grid[row][column].getToken() !== " ") {
+            // console.log("Wrong Move!!!");
+            return move;
         }
 
         grid[row][column].addToken(token);
-        return true;
+        return move+1;
     };
 
-    const printGrid = () => {
-        const markedGrid = grid.map((row) => row.map((cell) => cell.getToken()));
-        console.log(markedGrid);
-    };
+    // const printGrid = () => {
+    //     const markedGrid = grid.map((row) => row.map((cell) => cell.getToken()));
+    //     console.log(markedGrid);
+    // };
 
-    return { getGrid, markMove, printGrid };
+    return { getGrid, markMove,  };  // printGrid added for console.
 }
 
 function cell() {
-    let token = "_";
+    let token = " ";
 
     const addToken = (symbol) => {
         token = symbol;
@@ -39,6 +39,17 @@ function cell() {
     const getToken = () => token;
 
     return { addToken, getToken };
+}
+
+function checkWin(token, grid) {
+    return ((grid[0][0].getToken()==token && grid[0][1].getToken()==token && grid[0][2].getToken()==token) ||
+            (grid[1][0].getToken()==token && grid[1][1].getToken()==token && grid[1][2].getToken()==token) ||    
+            (grid[2][0].getToken()==token && grid[2][1].getToken()==token && grid[2][2].getToken()==token) ||   
+            (grid[0][0].getToken()==token && grid[1][0].getToken()==token && grid[2][0].getToken()==token) ||   
+            (grid[0][1].getToken()==token && grid[1][1].getToken()==token && grid[2][1].getToken()==token) ||   
+            (grid[0][2].getToken()==token && grid[1][2].getToken()==token && grid[2][2].getToken()==token) ||   
+            (grid[0][0].getToken()==token && grid[1][1].getToken()==token && grid[2][2].getToken()==token) ||    
+            (grid[0][2].getToken()==token && grid[1][1].getToken()==token && grid[2][0].getToken()==token));   
 }
 
 function gameController(
@@ -66,28 +77,146 @@ function gameController(
 
     const getActivePlayer = () => activePlayer;
 
-    const prindNewGrid = () => {
-        grid.printGrid();
-        console.log(`${getActivePlayer().name}'s turn...`);
-    }
+    // const prindNewGrid = () => {
+    //     grid.printGrid();
+    //     console.log(`${getActivePlayer().name}'s turn...`);
+    // }
 
-    const playRound = (row, column) => {
-        console.log(`${getActivePlayer().name} marked ${row} row, ${column} column...`);
-        let moved = grid.markMove(row, column, getActivePlayer().symbol);
-        
-        if (moved) {
+    let move = 0;
+
+    const getMove = () => move;
+
+    const playRound = (selectedCell, stateOfGame) => {
+        if (!stateOfGame)
+            return "";
+
+        // console.log(`${getActivePlayer().name} marked ${row} row, ${column} column...`);
+        let updatedMove = grid.markMove(selectedCell[0], selectedCell[1], getActivePlayer().symbol, getMove());
+
+        if (updatedMove >= 5 && checkWin(getActivePlayer().symbol, grid.getGrid())){
+            // console.log(`${getActivePlayer().name} is the Winner!!!`);
+            // grid.printGrid();
+            move = updatedMove;
+            return "win";
+        }
+        else if (updatedMove == 9) {
+            // console.log("Tie!");
+            move = updatedMove;
+            return "tie";
+        }
+        else if (updatedMove > getMove()) {
             switchActivePlayer();
+            move = updatedMove;
+            return "continue";
         }
         else {
-            console.log(`Retry ${getActivePlayer().name}`);
+            // console.log(`Retry ${getActivePlayer().name}`);
+            return "retry";
         }
 
-        prindNewGrid();
+        // prindNewGrid();
     };
 
-    prindNewGrid();
+    // prindNewGrid();
 
-    return { getActivePlayer, playRound };
+    return { getActivePlayer, playRound, getGrid : grid.getGrid() };
 }
 
-const game = gameController();
+function screenController (firstName, secondName) {
+    const game  = gameController(firstName, secondName);
+    const playerTurnDiv = document.querySelector(".turn");
+    const gridDiv = document.querySelector(".grid");
+    const grid = game.getGrid;
+    
+    const setGrid = () => {
+        gridDiv.innerHTML = "";
+        const activePlayer = game.getActivePlayer();
+        
+        playerTurnDiv.innerHTML = `${activePlayer.name}'s turn...`;
+        
+        grid.forEach((row, rowIndex) => {
+            row.forEach((cell, columnIndex) => {
+                const cellButton = document.createElement("button");
+                cellButton.classList.add("cell");
+                
+                cellButton.dataset.row = rowIndex;
+                cellButton.dataset.column = columnIndex;
+                
+                cellButton.innerHTML = cell.getToken();
+                gridDiv.appendChild(cellButton);
+            });
+        });
+    }
+
+    let stateOfGame = true;
+
+    const updateScreen = (action, e, selectedCell) => {
+        const activePlayer = game.getActivePlayer();
+        console.log(action);
+
+        playerTurnDiv.innerHTML = ((action == "continue")?`${activePlayer.name}'s turn...` : ((action == "win") ? `${activePlayer.name} is the Winner!!!`: ((action == "tie") ? "The match is a Tie." : `Retry ${activePlayer.name}`)));
+
+        e.target.innerHTML = grid[selectedCell[0]][selectedCell[1]].getToken();
+
+        if (action == "win" || action == "tie") {
+            stateOfGame = false;
+            const restartBtn = document.querySelector(".restart")
+            setTimeout(() => {
+                playerTurnDiv.classList.toggle("inactive");
+                restartBtn.classList.toggle("inactive");
+            }, 3000);
+        }
+    };
+
+    function clickHandlerGrid(e) {
+        const selectedCell = [e.target.dataset.row, e.target.dataset.column];
+
+        if (selectedCell.length == 0) return;
+
+        const action = game.playRound(selectedCell, stateOfGame);
+
+        if (action == "")
+            return;
+        updateScreen(action, e, selectedCell);
+    }
+
+    gridDiv.addEventListener("click", clickHandlerGrid);
+
+    setGrid();
+}
+
+function landingPage() {
+    const firstName = document.querySelector("#firstName");
+    const secondName = document.querySelector("#secondName");
+    const landingPage = document.querySelector(".landingPage");
+    const container = document.querySelector(".container");
+    const start = document.querySelector(".start");
+    const restart = document.querySelector(".restart");
+
+    const startMatch = () => {
+        if (firstName.checkValidity() && secondName.checkValidity()) {
+            landingPage.classList.toggle("inactive");
+            container.classList.toggle("inactive");
+
+            screenController(firstName.value, secondName.value);
+        }
+        else {
+            alert("Fill both Names.")
+        };
+    };
+
+    const startAgain = () => {
+        firstName.value = '';
+        secondName.value = '';
+        const playerTurnDiv = document.querySelector(".turn");
+        landingPage.classList.toggle("inactive");
+        container.classList.toggle("inactive");
+        restart.classList.toggle("inactive");
+        playerTurnDiv.classList.toggle("inactive");
+    };
+
+    start.addEventListener("click" , startMatch);
+    restart.addEventListener("click", startAgain);
+}
+
+landingPage();
